ea2fd0085521663a9e922d699ad87a99de31ae1c
joelb <joelbauer360@hotmail.com>
2023-03-29 22:29:38 -0300

    actualizado

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CartWidget from '../CartWidget';
+
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav_link" href="#">BauerShop</a>
+                </div>
+                <ul className="nav_list">
+                    <li>
+                        <a className="nav_link" href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">
+                            <CartWidget />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
